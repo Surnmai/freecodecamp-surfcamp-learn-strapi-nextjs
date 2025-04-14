5d77fea9d1b87e6c130baf2804137ed738fc039a
Surnmai <surnmai1@gmail.com>
2025-04-14 13:21:41 +0000

    trying diect fetch

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,15 +1,52 @@
 import { getHomePage } from "@/data/loaders";
 import { notFound } from "next/navigation";
+// import qs from "qs";
+
+// trying without the Strapi fetch
+// const homePageQuery = qs.stringify({
+//   populate: {
+//     blocks: {
+//       on: {
+//         "blocks.hero-section": {
+//           populate: {
+//             image: {
+//               fields: ["url", "alternativeText"],
+//             },
+//             logo: {
+//               populate: {
+//                 image: {
+//                   fields: ["url", "alternativeText"],
+//                 },
+//               },
+//             },
+//             cta: true,
+//           },
+//         },
+//         "blocks.info-block": {
+//           populate: {
+//             image: {
+//               fields: ["url", "alternativeText"],
+//             },
+//             cta: true,
+//           },
+//         },
+//       },
+//     },
+//   },
+// });
 
 const loader = async () => {
   // const path = "/api/home-page";
   // const BASE_URL = "http://localhost:1337/api/home-page";
   // const url = "http://127.0.0.1:1337/api/home-page";
   // const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:1337";
+
   // const url = new URL(path, BASE_URL);
+  // url.search = homePageQuery;
 
   // const res = await fetch(url);
   // const data = await res.json();
+  // if (!data) notFound;
   // console.log(data);
 
   // return { ...data.data };
